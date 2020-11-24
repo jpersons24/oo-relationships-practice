310130ded0b78109dab1304dfcce09a660c8852d
Jakob Persons <jakobep24@gmail.com>
2020-11-24 08:55:26 -0500

    saved driver

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -14,4 +14,25 @@ class Driver
         @@all
     end
 
+    # returns all rides a Driver has made
+    def rides
+    end
+
+    # returns all passengers associated with the Driver
+    def passengers
+    end
+
+    # returns array of distances for each ride Driver has made
+    def distances
+    end
+
+    # calculates and returns the total miles driven by Driver
+    def total_distance
+    end
+
+    # takes argument of distance = floating number
+    # returns all dirvers who have exceeded distance argument
+    def self.mileage_cap(distance)
+    end
+
 end
